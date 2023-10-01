1d859d3847358cf3577693404c8db1f2d5aa7753
roritopra <1193102598@u.icesi.edu.co>
2023-10-01 14:20:20 -0500

    [FIX] Fixed screens for vercel

--- src/Item.ts
+++ src/Item.ts
@@ -17,11 +17,11 @@ export class Item {
   velY: number;
 
   constructor(p:p5) {
-    this.cdUrl = 'assets/level 2/cd.png';
-    this.nailUrl = 'assets/level 2/nail poslish.png';
-    this.lipstickUrl = 'assets/level 2/lipstick.png';
-    this.sockUrl = 'assets/level 2/sock.png';
-    this.condomUrl = 'assets/level 2/condom.png';
+    this.cdUrl = '/src/assets/level 2/cd.png';
+    this.nailUrl = '/src/assets/level 2/nail poslish.png';
+    this.lipstickUrl = '/src/assets/level 2/lipstick.png';
+    this.sockUrl = '/src/assets/level 2/sock.png';
+    this.condomUrl = '/src/assets/level 2/condom.png';
     this.cd = p.loadImage(this.cdUrl);
     this.nail = p.loadImage(this.nailUrl);
     this.lipstick = p.loadImage(this.lipstickUrl);
